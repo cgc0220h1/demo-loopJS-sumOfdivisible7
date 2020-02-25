60341b846bf3ad61c14e76affa75bd0e8cf6d17f
vanduc2514 <vanduc2514@gmail.com>
2020-02-25 17:25:04 +0700

    Sua lai code

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 function sumOfdivide7() {
     let count = 0;
     let sum = 0;
-    for (let index = 1; count < 31 ; index++) {
+    let index = 1;
+    for (index; count < 31 ;) {
         if (index % 7 === 0) {
             sum += index;
             count++;
+            index++;
         }
         index++;
     }
